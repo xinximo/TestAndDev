461ee2ed3e62414eba1311ffb907dd4cb3f9af20
xinximo <814037312@qq.com>
2021-03-14 19:19:07 +0800

    测试上传

--- test_study/test_selenium.py
+++ test_study/test_selenium.py
@@ -1,9 +1,14 @@
+import allure
 import selenium
 from selenium import webdriver
 
+@allure.feature("测试类")
+class Test_Demo():
+    @allure.story("测试用例1")
+    def test_selenium(self):
+        driver = webdriver.Chrome()
+        driver.get("http://www.baidu.com")
 
-def test_selenium():
-    driver = webdriver.Chrome()
-    driver.get("http://www.baidu.com")
-def test_wx1():
-    pass
+    @allure.story("测试用例2")
+    def test_wx1(self):
+        pass
