3eb6af14b39c753b40fafcbc0246138e16343b26
xinximo <814037312@qq.com>
2021-03-21 17:06:21 +0800

    grid

--- test_study/test_selenium.py
+++ test_study/test_selenium.py
@@ -1,14 +1,33 @@
 import allure
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 @allure.feature("测试类")
 class Test_Demo():
     @allure.story("测试用例1")
     def test_selenium(self):
-        driver = webdriver.Chrome()
+        driver = webdriver.Remote(
+            command_executor='http://localhost:5001/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
+        #driver = webdriver.Chrome()
         driver.get("http://www.baidu.com")
+        driver.close()
 
     @allure.story("测试用例2")
     def test_wx1(self):
         pass
+
+    @allure.story("测试用例1")
+    def test_selenium2(self):
+        driver = webdriver.Remote(
+            command_executor='http://localhost:5001/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
+        # driver = webdriver.Chrome()
+        driver.get("http://www.baidu.com")
+        driver.close()
+
+    @allure.story("测试用例2")
+    def test_wx2(self):
+        pass
+    #并发测试 pytest -n 3 test_selenium.py
